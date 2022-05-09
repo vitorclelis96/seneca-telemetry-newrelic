10dc02d1a24daaf53df35ffd44f66c938b60570f
Gustavo <guhmerces@hotmail.com>
2022-05-09 04:38:44 -0300

    Wrap actions in order to create newrelic Segments

--- src/newrelic-provider.ts
+++ src/newrelic-provider.ts
@@ -1,6 +1,59 @@
 /* Copyright © 2021 Seneca Project Contributors, MIT License. */
+import newrelic from 'newrelic';
 
 type NewRelicProviderOptions = {};
+
+type Spec = Record<any, any>
+
+function changeSpec(spec: Spec, event: "inward" | "outward") {
+    return {
+        addSegments: () => {
+            if(spec.ctx.actdef?.func) {
+                const { func, pattern }: Record<string, any> = spec.ctx.actdef
+
+                spec.ctx.actdef.func = (...args: any) => {
+                    newrelic.startSegment(
+                        pattern,
+                        true,
+                        function handler(endSegment) {
+                            func(...args)
+                            // include endSegment to context
+                            let context = spec.ctx.seneca.context
+                            context.shared = context.shared || {}
+                            context.shared.endSegment = (context.shared.endSegment || endSegment)
+                        },
+                        function endSegment() {
+                            console.log('end for action of pattern ' + spec.ctx.actdef.pattern)                          
+                        }
+                    )
+                }
+
+            }
+        },
+        endSegments: () => {
+            let context = spec.ctx.seneca.context
+            const endSegmentCb = context.shared?.endSegment
+            if(endSegmentCb) {
+                endSegmentCb()
+            }
+        },
+        extractFromSpec: () => {
+
+        }
+    }
+}
+
+function PreloadNewrelicProvider(this: any, opts: any) {
+    const seneca = this
+
+    seneca.order.inward.add((spec: Spec) => {
+        changeSpec(spec, 'inward').addSegments()
+    })
+
+    seneca.order.outward.add((spec: Spec) => {
+        changeSpec(spec, 'outward').endSegments()
+    })
+}
 
 function NewrelicProvider(this: any, _options: any) {
     const seneca: any = this
@@ -8,8 +61,17 @@ function NewrelicProvider(this: any, _options: any) {
 
     // NOTE: sys- zone prefix is reserved.
 
+    const sleep = (millis: number) => new Promise(r=>setTimeout(r,millis))
+
+
     seneca
         .message('sys:provider,provider:newrelic,get:info', get_info)
+        .message('m:1', async function m1(msg: any) {
+            console.log('inside m1');
+            await sleep(3000)
+            const m = msg.m
+            return { m: m * 2 }
+        })
 
 
     async function get_info(this: any, _msg: any) {
@@ -49,4 +111,5 @@ export default NewrelicProvider
 
 if ('undefined' !== typeof (module)) {
     module.exports = NewrelicProvider
+    module.exports.preload = PreloadNewrelicProvider
 }
